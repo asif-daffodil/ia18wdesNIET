f2f4cb076c06569ef75f0817f05aad4c796c88d4
Asif Abir <abir@dipti.com.bd>
2022-03-20 17:50:19 +0600

    mar-20-2022

---
+++ mar-6-2022/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+    <ul class="menubar">
+        <li>
+            <a href="">Home</a>
+        </li>
+        <li>
+            <a href="">About</a>
+        </li>
+        <li>
+            <a href="">Gallery</a>
+        </li>
+        <li>
+            <a href="">Blog</a>
+        </li>
+        <li>
+            <a href="">Contact</a>
+        </li>
+    </ul>
+
+    <div class="sumi">
+        <div class="rumi"></div>
+        <div class="omi"></div>
+        <div class="fahad"></div>
+    </div>
+
+    <div class="mehedi">
+        <div class="sadia"></div>
+        <div class="unknownGirl"></div>
+    </div>
+</body>
+</html>
